a9bd2b1b7bbf1e9c1f31bfcdc0c9409109583d3d
EricFernandezSnyk <eric.fernandez@snyk.io>
2022-07-15 16:18:17 +0100

    Fixed error and added tests

--- tests/test_snyk_tags.py
+++ tests/test_snyk_tags.py
@@ -1,5 +1,15 @@
-from snyk_tags import __version__
+from snyk_tags import __version__, tags
+from typer.testing import CliRunner
 
+runner = CliRunner()
+app = tags.app
+
+def test_help():
+    result = runner.invoke(app, ["--help"])
+    assert result.exit_code == 0
 
 def test_version():
-    assert __version__ == '0.4.1'
+    result = runner.invoke(app, ["--version"])
+    assert result.exit_code == 0
+    assert f"snyk-tags v{__version__}\n" in result.stdout
+    

fa744c4ce9d0470a850135f1222a4abefd859cc9
Casey Marshall <casey.marshall@snyk.io>
2023-07-25 18:23:34 -0500

    chore: add library functions supporting new component command
    Library functions supporting an upcoming `component` command, which will
    support bulk tagging large amounts of projects in an org to define
    software components according to rules.
    
    These functions define the rules schema and execution as filters which
    will operate over extended project resource attributes.
    
    An API client layer is introduced, which will initially be used for the
    new `component` command, but intended for use in other commands with
    followup refactoring. The API client layer supports automatic backoff
    retries with jitter, so that large bulk operations are not trivially
    disrupted by network issues or rate limits.

---
+++ tests/lib/component_rules/test_matcher.py
@@ -0,0 +1,88 @@
+from dataclasses import dataclass
+
+from snyk_tags.lib.component_rules import model
+from snyk_tags.lib.component_rules.matcher import object_matcher
+
+
+def test_string_matcher():
+    cfg = model.parse_rules(
+        r"""
+version: 1
+rules:
+  - name: specific-project
+    projects:
+      - target:
+          url: 'http://github.com/snyk/repo1'
+    component: pkg:github/snyk/something-completely-stringy
+"""
+    )
+    context = {}
+    match_fn = object_matcher(cfg["rules"][0]["projects"][0], context)
+
+    @dataclass
+    class TestCase:
+        project: any
+        match: bool
+
+    testcases = [
+        TestCase({}, False),
+        TestCase({"target": "nope"}, False),
+        TestCase({"target": {"url": "nope"}}, False),
+        TestCase({"target": {"url": "http://github.com/snyk/repo1"}}, True),
+    ]
+    for testcase in testcases:
+        is_match = match_fn(testcase.project)
+        assert is_match == testcase.match
+
+
+def test_regex_matcher():
+    cfg = model.parse_rules(
+        r"""
+version: 1
+rules:
+  - name: specific-project
+    projects:
+      - name:
+          regex: ':foo:'
+    component: pkg:github/snyk/something-completely-fooey
+"""
+    )
+    context = {}
+    match_fn = object_matcher(cfg["rules"][0]["projects"][0], context)
+
+    @dataclass
+    class TestCase:
+        project: any
+        match: bool
+
+    testcases = [
+        TestCase({}, False),
+        TestCase({"name": "nope"}, False),
+        TestCase({"name": {"foo": "foo"}}, False),
+        TestCase({"name": ":foo:bar"}, True),
+        TestCase({"name": "bar:foo:"}, True),
+    ]
+    for testcase in testcases:
+        is_match = match_fn(testcase.project)
+        assert is_match == testcase.match
+
+def test_regex_context_matcher():
+    cfg = model.parse_rules(
+        r"""
+version: 1
+rules:
+  - name: specific-project
+    projects:
+      - name:
+          regex: 'docker-image[|](?P<image>\w+):(?P<tag>\w+)$'
+    component: pkg:github/snyk/something-completely-fooey
+"""
+    )
+    context = {}
+    match_fn = object_matcher(cfg["rules"][0]["projects"][0], context)
+    is_match = match_fn({
+      "name": "docker-image|postgres:14",
+    })
+    assert is_match == True
+    assert context.get('image') == 'postgres'
+    assert context.get('tag') == '14'
---
+++ tests/lib/component_rules/test_model.py
@@ -0,0 +1,72 @@
+from dataclasses import dataclass
+
+import jsonschema
+import pytest
+
+from snyk_tags.lib.component_rules import model
+
+
+def test_model_parse():
+    m = model.parse_rules(r"""
+version: 1
+rules:
+  - name: specific-project
+    projects:
+      - target:
+          url: 'http://github.com/snyk/repo1'
+      - target:
+          url: 'http://github.com/snyk/repo2'
+      - target:
+          url: 'http://github.com/snyk/repo3'
+      - origin: kubernetes
+        target:
+          display_name:
+            regex: '.*/deployment\.apps/some-image$'
+      - origin: ecr
+        target:
+          display_name:
+            regex: '^some-image:[0-9a-f]+$'
+      - origin: cli
+        name:
+          regex: '.*\.amazonaws\.com/some-image$'
+    component: pkg:github/snyk/something-completely-different
+""")
+    assert m is not None
+    assert m['version'] == 1
+    assert m['rules'][0]['component'] == "pkg:github/snyk/something-completely-different"
+
+
+def test_jsonschema_constraints():
+    @dataclass
+    class TestCase:
+        match: str
+        yaml: str
+    testcases = [
+        TestCase("'' is not of type 'object'", "''"),
+        TestCase("Failed validating 'required' in schema", "{}"),
+        TestCase("Failed validating 'minItems' in schema", """
+version: 1
+rules: []
+"""),
+        TestCase("Failed validating 'minLength' in schema", """
+version: 1
+rules:
+  - name: foo
+    projects:
+      - name: ''
+    component: 'bar'
+"""),
+        TestCase("'regex' is a required property", """
+version: 1
+rules:
+  - name: foo
+    projects:
+      - target:
+          url: {}
+    component: 'baz'
+"""),
+    ]
+
+    for testcase in testcases:
+        with pytest.raises(jsonschema.ValidationError, match=testcase.match):
+            model.parse_rules(testcase.yaml)

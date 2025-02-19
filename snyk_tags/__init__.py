4df65fe5e09cd810f26ee0bd4f0cdf5093b64435
Grant Murphy <grant.murphy@snyk.io>
2025-02-19 14:03:46 +1000

    fix: keep __version__ sync with pyproject.toml

--- snyk_tags/__init__.py
+++ snyk_tags/__init__.py
@@ -1,7 +1,13 @@
 # snyk_tags/__init__.py
 
 __app_name__ = "snyk_tags"
-__version__ = "2.5.0"
+try:
+    from importlib.metadata import version
+    __version__ = version(__app_name__)
+
+except ImportError:
+    __version__ = "development"
+
 
 from logging import ERROR
 from sre_constants import SUCCESS
